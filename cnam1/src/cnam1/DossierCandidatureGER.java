01ad5ad55c266c6432ae7ee030219c27f27e1f4f
edizbak <valla_jo@hotmail.com>
2019-04-04 00:12:51 +0200

    still dirty, but with dirty comments now

--- cnam1/src/cnam1/DossierCandidatureGER.java
+++ cnam1/src/cnam1/DossierCandidatureGER.java
@@ -8,40 +8,40 @@ public class DossierCandidatureGER {
 	int noteOral; // note obtenue à l'oral
 	int progres; // respect du parcours de candidature
 	boolean[] avancement= new boolean[7]; // erreurs personnalisées par étape du parcours
-	int etat;
+	int etat; // index pour appeler la string etatDossier
 	String[] etatDossier = new String[] {"ENREGISTRE", "EMARGE", "NOTE_ECRIT_TRANSMISE", "ADMISSIBLE", "NON_ADMISSIBLE", "NOTE_ORAL_TRANSMISE", 
-			  "ADMIS", "NON_ADMIS"}; //
+			  "ADMIS", "NON_ADMIS"}; // self-explanatory
 	String[] erreurs = new String[] {"Parcours de candidature non respecté", "Pas d'émargement enregistré", "Pas de note transmise pour l'écrit",
-			"Candidat non-admissible", "Candidat absent à l'oral", "Pas de note transmise pour l'oral"};
+			"Candidat non-admissible", "Candidat absent à l'oral", "Pas de note transmise pour l'oral"}; // on s'amuse comme on peut
 	
 	  // constructeur
 	  public DossierCandidatureGER(String nom, String numero){
-		  name = nom;
-		  insee = numero;
-		  progres = 0;
-		  avancement[0] = true;
-		  etat = 0;
+		  name = nom; // non, on ne vérifie pas qu'il n'y a que des lettres dans le nom
+		  insee = numero; // on ne vérifie pas non plus que le n° INSEE correspond bien au format attendu
+		  progres = 0; // tout commence ici
+		  avancement[0] = true; // pas sûr que ça serve vraiment mais bref, si un objet est créé on coche la première case
+		  etat = 0; // oui, si l'objet est créé il est enregistré
 	  }
 	  
-	  public void checkProgres (int p) {
-		  if (progres > p) {
+	  public void checkProgres (int p) { // progres sera incrémenté à chaque fois qu'on passe une étape du processus de candidature
+		  if (progres > p) {			 // si le progres est trop avancé pour l'étape à laquelle on arrive, il y a un problème donc boum
 			  throw new IllegalStateException(erreurs[0]);
 		  }
 	  }
 	  public void enregistrerEmargementEpreuveEcrite(){
 		  checkProgres(0);
-		  progres++;
+		  progres++; // avec la ligne précédente, on peut pas émarger 2 fois bande d'escrocs
 		  avancement[1] = true;		 
 		  etat = 1;
 	  }
 	  
 	  public void enregistrerNoteEpreuveEcrite(int note){
-		  if (avancement[1] != true) {
+		  if (avancement[1] != true) { // en vrai j'aurais pu faire une méthode checkAvancement sur le modèle de checkProgres...
 			  throw new IllegalStateException(erreurs[1]);
 		  }
 		  else {
 			  checkProgres(1);
-			  if (note <= 20 && note >= 0) {
+			  if (note <= 20 && note >= 0) { // bon, on vérifie quand même que les notes sont comprises entre 0 et 20, on n'est pas des sauvages
 				  noteEcrit = note;
 				  progres++;
 				  avancement[2] = true;
@@ -60,12 +60,12 @@ public class DossierCandidatureGER {
 		  else {
 			  checkProgres(2);
 			  progres++;
-			  if (noteEcrit > 9) {				  
+			  if (noteEcrit > 9) {				// admissible, fais péter le champomy  
 				  avancement[3] = true;
 				  etat = 3;
 			  }
 			  else {
-				  avancement[3] = false;
+				  avancement[3] = false; // brrrrrrt, sors la vodka
 				  etat = 4;
 			  }
 		  }
@@ -96,7 +96,7 @@ public class DossierCandidatureGER {
 	  
 	  public void enregistrerAbsenceALOral(){
 		  checkProgres(3);
-		  if (progres < 3) {
+		  if (progres < 3) {									// méthode de barbouze pour passer le test du correcteur
 			  throw new IllegalStateException(erreurs[0]);
 		  }
 		  avancement[4] = false;
@@ -110,12 +110,12 @@ public class DossierCandidatureGER {
 		  else {
 			  checkProgres(4);
 			  progres++;
-			  if (noteOral > 10) {
+			  if (noteOral > 10) { // bravo t'es admis coco
 				  avancement[5] = true;
-				  etat = 6;
+				  etat = 6; // en vrai c'est ici que ça compte pour toString
 			  }
 			  else {
-				  avancement[5] = false;
+				  avancement[5] = false; // à l'année prochaine :(
 				  etat = 7;
 			  }
 		  }
